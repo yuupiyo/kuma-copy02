ea3aa7cefd159c8e6e72602b069aeace999e0be4
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2024-12-24 09:34:28 +0900

    loading/一度コメントアウトする

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// loading
+// window.addEventListener('load', function() {
+//   const loadingElement = document.querySelector('.js-loading');
+//   if (loadingElement) {
+//       loadingElement.style.display = 'none';
+//   }
+// });
